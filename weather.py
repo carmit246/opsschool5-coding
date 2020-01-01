509f45b4f51bcb6989c7a40af7d3203444f19d2e
Carmit Danon <carmitbe1@gmail.com>
2020-01-01 11:13:20 +0200

    fix HW

--- weather.py
+++ weather.py
@@ -4,12 +4,20 @@ from requests import get
 
 
 def get_data(city):
-    url = "http://api.weatherstack.com/current?access_key=605428999402e46807e8cb3b93e67c7b&query=" + city
+    token_file = open('token.txt')
+    token = token_file.read()
+    url = "http://api.weatherstack.com/current?access_key={}{}".format(token, city)
     rest_result = get(url)
     return rest_result.json()
 
 
 def get_multiple_arguments():
+    if len(sys.argv) < 3:
+        print("usage: weather.pl <city list-comma separated> <degrees:-s|-f>.\n Example: weather.py Paris,London -f")
+        sys.exit(1)
+    if sys.argv[2] != '-f' and sys.argv[2] != '-c':
+        print("usage: weather.pl <city list-comma separated> <degrees:-s|-f>.\n Example: weather.py Paris,London -f")
+        sys.exit(1)
     cities = sys.argv[1].split(',')
     degrees = sys.argv[2]
     return cities, degrees
@@ -20,11 +28,11 @@ def celsius_to_fehrenheit(current_temp):
 
 
 def result_by_degree(city, current_temp, degrees):
-    if degrees == "f":
+    if degrees == "-f":
         current_temp = celsius_to_fehrenheit(current_temp)
-        result = "The weather in " + city + " today " + str(int(current_temp)) + " Fahrenheit."
+        result = "The weather in {} today is {} Fahrenheit.".format(city, current_temp)
     else:
-        result = "The weather in " + city + " today " + str(current_temp) + " celsius."
+        result = "The weather in {} today is {} celsius.".format(city, current_temp)
     return result
 
 
